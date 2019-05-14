5f74c47f23b7d1a4d7f61752eb813d9158936f46
mery_cherrry@outlook.com <mery_cherrry@outlook.com>
2019-05-14 14:58:35 +0200

    redirect to order success page, code refactor

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -4,6 +4,8 @@ import { ShoppingCartService } from '../services/shopping/shopping-cart.service'
 import { ShoppingCart } from '../models/shopping-cart';
 import { Subscription } from 'rxjs';
 import { OrderService } from '../services/order/order.service';
+import { Order } from '../models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'check-out',
@@ -18,6 +20,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   userId: string;
 
   constructor(
+     private router: Router,
      private authService: AuthService,
      private shoppingCartService: ShoppingCartService,
      private orderService: OrderService ) { }
@@ -33,23 +36,9 @@ export class CheckOutComponent implements OnInit, OnDestroy {
     this.userSubscription.unsubscribe();
   }
 
-  placeOrder() {
-    let order = {
-      userId: this.userId,
-      datePlaced : new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map( i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        };
-      })
-    };
-    this.orderService.storeOrder(order);
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.placeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
   }
 }
